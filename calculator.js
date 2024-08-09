d31007d7d465b2e4c1671823d71ebf6d2c7a4e09
Greg Demko <gregory.demko@my.uwrf.edu>
2024-08-09 14:28:48 -0500

    Make an operate function that will call the other functions based on what operator the user selected

--- calculator.js
+++ calculator.js
@@ -1,7 +1,17 @@
 function add(a,b) {a+b}
-
 function subtract(a,b) {a-b}
-
 function divide(a,b) {a/b}
-
 function multiply(a,b) {a*b}
+
+function operate(a, b, operator){
+    if (operator === "+") add(a,b);
+    else if (operator === "-") subtract(a,b);
+    else if (operator === "/") divide(a,b);
+    else if (operator === "*") multiply(a,b);
+
+    else alert("invalid operator!!");
+}
+
+let a;
+let b;
+let operator;
